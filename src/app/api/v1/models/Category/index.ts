25011aa57531f2df48ca890ee483133faa351e5b
Sergey Vatagin <s.vatagin@gmail.com>
2022-04-08 18:27:41 +0300

    Add update dynamic query build

--- src/app/api/v1/models/Category/index.ts
+++ src/app/api/v1/models/Category/index.ts
@@ -24,12 +24,12 @@ interface UpdateCategoryDto {
   active?: boolean;
 }
 
-// const baseHandler = async
 
 export const getAll = async ():Promise<Category[]> => {
   const result = await db.query('SELECT * from categories');
   return result?.rows; 
 }
+
 
 export const getOne = async (id: string):Promise<Category[]> => {
   const selectOneQuery = 'SELECT * from categories WHERE id = $1';
@@ -37,19 +37,28 @@ export const getOne = async (id: string):Promise<Category[]> => {
   return result?.rows; 
 }
 
+
 export const create = async (params: CreateCategoryDto):Promise<any> => {
   const insertQuery = 'INSERT INTO categories(slug, name, description, active) VALUES($1, $2, $3, $4) RETURNING id'
   const result = await db.query(insertQuery, Object.values(params));
   return result?.rows; 
 }
 
+
 export const update = async (id: string, params: UpdateCategoryDto):Promise<any> => {
+  const dKeys = Object.keys(params);
+  const dataTuples = dKeys.map((k, index) => `${k} = $${index + 1}`);
+  const updates = dataTuples.join(", ");
+  const len = Object.keys(params).length;
+
+  let updateQuery = `UPDATE categories SET ${updates} WHERE id = $${len + 1} RETURNING id`;
+
   let paramsArray = Object.values(params)
   paramsArray.push(id);
-  const updateQuery = 'UPDATE categories SET (slug, name, description, active) = ($1, $2, $3, $4) WHERE id=($5) RETURNING id'
   const result = await db.query(updateQuery, paramsArray);
   return result?.rows; 
 }
+
 
 export const destroy = async (id: string):Promise<any> => {
   const deleteQuery = 'DELETE FROM categories WHERE id=($1) RETURNING id'
